841c50dd6ce7b71d3287ed736594437e2dd3ab2d
Sébastien ALEXANDRE <sebastien@graphidev.fr>
2016-11-15 15:40:57 +0100

    Implements first fonctionnal release of the File component

--- src/Components/File.php
+++ src/Components/File.php
@@ -2,25 +2,74 @@
 
     namespace WOK\HttpMessage\Components;
 
-    use Psr\Http\Message\UploadedFileInterface;
-
-    class File implements \UploadedFileInterface {
-
+    class File {
 
         /**
-         * @var     array       $file       File meta data
+         * @var   string            $path        Uploaded file path
         **/
-        protected $file = array();
+        protected $path = array();
+
+        /**
+         * @var   null|int          $size         File client size
+        **/
+        protected $size = null;
+
+        /**
+         * @var   string            $type         File client media type
+        **/
+        protected $type = null;
+
+        /**
+         * @var   string            $type         File client media type
+        **/
+        // protected $type = null;
 
 
         /**
          * Instanciate a file object
          * @param   array       $file          File meta data
         **/
-        public function __construct(array $file) {
+        public function __construct($path, $name, $type = null, $size = null, $error = null) {
 
-            $this->file = $file;
+            $this->path     = $path;
+            $this->name     = $name;
+            $this->type     = $type;
+            $this->size     = ($size ?: null);
+            $this->error    = ($error ?: null);
 
         }
+
+
+        /**
+         * Retrieve the error associated to the file
+         * @return  integer     Returns one of the UPLOAD_ERR_XXX constants
+        **/
+        public function getError() {
+
+            return $this->file['error'];
+
+        }
+
+
+        public function getClientFilename() {
+
+            return $this->file['name'];
+
+        }
+
+
+        public function getClientMediaType() {
+
+            return $this->file['type'];
+
+        }
+
+
+        public function getClientSize() {
+
+            return $this->file['size'];
+
+        }
+
 
     }
--- src/Components/FilesCollection.php
+++ src/Components/FilesCollection.php
@@ -14,30 +14,60 @@
         **/
         public function __construct(array $files = array()) {
 
-            // @see notes in <http://php.net/manual/fr/reserved.variables.files.php>
-            /*
+            $list = array();
+            foreach($files as $key => $file) {
 
-                $_FILES = [
+                // Multiple unordered files
+                if(isset($file['name']) && is_array($file['name'])) {
 
-                ]
+                    $list[$key] = array();
+                    foreach($file['name'] as $i => $value) {
 
-            */
-            $files = $this->_orderFilesList($files);
+                        $list[$key][] = new File(
+                            $files[$key]['tmp_name'][$i],
+                            $files[$key]['name'][$i],
+                            $files[$key]['type'][$i],
+                            $files[$key]['size'][$i],
+                            $files[$key]['error'][$i]
+                        );
 
-            parent::__construct($files);
+                    }
+
+                }
+
+                // Already ordered multiple files (supposed)
+                elseif(!isset($file['name'])) {
+
+                    $list[$key] = array();
+                    foreach($file as $item) {
+
+                        $list[$key][] = new File(
+                            $item['tmp_name'],
+                            $item['name'],
+                            $item['type'],
+                            $item['size'],
+                            $item['error']
+                        );
+
+                    }
+
+                }
+
+                // Standalone file
+                else {
+
+                    $list[$key] = new File(
+                        $file['tmp_name'], $file['name'], $file['type'], $file['size'], $file['error']
+                    );
+
+                }
+
+            }
+
+            parent::__construct($list);
 
         }
 
-        /**
-         * @note    This function reorder files list in a more usable one
-        **/
-        static public function createFromUnorderedList($files) {
-
-
-
-            return self::__construct($files);
-
-        }
 
         /**
          * Clone behavior
